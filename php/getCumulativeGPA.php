cc496a6ec870edc000a255727cd86f2a54d4aa09
Bill <hawkins5513@yahoo.com>
2016-06-06 22:48:12 -0400

    renamed getCumlative.php to getCumulativeGPA.php and started on getSemesterGPA.php adding queries to calculate GPA

---
+++ php/getCumulativeGPA.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<?php
+    include("config.php");
+
+     $email = $_GET['email'];
+     $year = $_GET['year'];
+     $semester = $_GET['semester'];
+        
+     if (    (isset($year) && $year != "") 
+          && ( isset($semester) && $semester != "")
+          && ( isset($email) && $email !="") ) {
+        session_start();
+        $callYearsAttended = $conn->prepare("CALL getYearsAttended(?)");
+        $callYearsAttended->bind_param('s', $_GET['email']);
+        $callYearsAttended->execute();      
+        $result = $callYearsAttended->fetch();
+        $yearsAttended = $result[//column name not sure
+        $callYearsAttended->close();        
+        
+        $call = $conn->prepare("CALL getTotalGPA(?, ?)"          
+        $call->bind_param('ss', $email, $yearsAttended);
+        $call->execute();
+        $result = call->fetch();
+        $cumulativeGPA = $result[//column name not sure        
+     }         
+?>
--- php/getSemesterGPA.php
+++ php/getSemesterGPA.php
@@ -8,20 +8,16 @@
         
      if ( (isset($year) && $year != "") && ( isset($semester) && $semester != "") ) {
         session_start();
-        IN email VARCHAR(30), 
-                                IN semester VARCHAR(6), 
-                                IN year INT(4), 
-                                INOUT gradeTotal INT(3), 
-                                INOUT creditHours INT(3)
+      
         $call = $conn->prepare("CALL getSemesterGPA(?, ?, @gradetotal, @credithours)"          
-        $call = $conn->bind_param('si', $_GET['year'], $_GET['semester']);
+        $call->bind_param('si', $_GET['year'], $_GET['semester']);
         $call->execute();
+        $call->close();
 
         $select = $conn->query("SELECT @gradetotal, @credithours");
         $result = $select->fetch_assoc();
         $GPA = ($result['@gradetotal'] / $result['@credithours']);
         
-        $call->close();
         $zero = 0;
         // Clear gradetotal variable
         $stmt = $conn->prepare('SET @gradetotal = ?, @credithours = ?');
