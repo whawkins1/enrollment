84afaee1558e30a99008b165996dcd13c93ec064
Bill <hawkins5513@yahoo.com>
2016-11-01 23:52:31 -0400

    Changed semester check in addcourse.php to timestamp check and used year instead of just month and day

--- php/addCourse.php
+++ php/addCourse.php
@@ -8,45 +8,35 @@
       
       $current_date = new simpleDate();
       $current_year = $current_date->now()->getYear();
-      $today_date_semester = new DateTime('Y-m-d H:i:s');
+      $today_date_semester = new DateTime();
+      $today_timestamp_semester = $today_date_semester->getTimestamp();
       
-      $fall_start_date = new DateTime("Sep 1");
-      $fall_end_date = new DateTime("Jan 14");
-        
-       if ($fall_start_date < $fall_end_date ) { 
-            $between_fall_date = ($fall_start_date <  $today_date_semster && $today_date_semester < $fall_end_date); 
-       } else if ($fall_start_date > $fall_end_date) {
-            $between_fall_date = ($today_date_semester < $fall_end_date || $today_date_semester > $fall_start_date);          
-       }           
+       $fall_start_date = new DateTime($current_year . "-09-01");
+       $fall_end_date = new DateTime($current_year . "-01-14");      
+      
+       if ($fall_start_date->getTimestamp() < $today_timestamp_semester &&
+           $fall_end_date->getTimestamp() > $today_timestamp_semester  ) {
+           $semester = "spring";               
+       }          
        
-       if (!(isset($between_fall_date))) {
-           $spring_start_date = new DateTime("Jan 15");
-           $spring_end_date = new DateTime("May 15");
+       if (!(isset($semester))) {
+           $spring_start_date = new DateTime($current_year . "-01-15");
+           $spring_end_date = new DateTime($current_year . "-05-15");
            
-           if ($spring_start_date < $spring_end_date) {
-              $between_spring_date = ($spring_start_date <= $today_date_semester && $today_date_semester <= $spring_end_date);
-           } else if ($spring_start_date > $spring_end_date) {
-              $between_spring_date = ($today_date_semester  <= $spring_end_date || $today_date_semester >= $spring_start_date);
-           }
+           if ($spring_start_date->getTimestamp() < $today_timestamp_semester &&
+               $spring_end_date->getTimestamp() > $today_timestamp_semester) {
+               $semester = "summer";
+           } 
        }
        
-       if (!(isset($between_fall_date) && isset($between_spring_date))) {
-            $summer_start_date = new DateTime("Jun 1");
-            $summer_end_date = new DateTime("Aug 30");
+       if (!(isset($semester))) {
+            $summer_start_date = new DateTime($current_year . "-06-01");
+            $summer_end_date = new DateTime($current_year . "-08-30");
             
-            if ($summer_start_date < $summer_end_date ) { 
-               $between_summer_date = ($summer_start_date <  $today_date_semester && $today_date_semester < $summer_end_date); 
-            } else if ($summer_start_date > $summer_end_date) {
-               $between_summer_date = ($today_date_semester < $summer_end_date || $today_date_semester > $summer_start_date);          
-            }    
-       }
-       
-       if ($between_fall_date) {
-            $current_semester = "spring";
-       } else if ($between_spring_date) {
-            $current_semester = "summer";
-       } else if ($between_summer_date) {
-            $current_semester = "fall";
+            if ($summer_start_date->getTimestamp() < $today_timestamp_semester &&
+                $summer_end_date->getTimestamp() < $today_timestamp_ ) { 
+                $semester = 'fall'; 
+            }     
        }
        
       $sql = "INSERT INTO enrolled_" . $current_year " user_email_" . $current_year .
