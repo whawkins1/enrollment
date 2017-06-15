f22d287abea438506c8e141d02659c418e423e48
Bill <hawkins5513@yahoo.com>
2017-06-15 15:19:27 -0400

    Removed Comments from findCourse.php

--- php/findCourse.php
+++ php/findCourse.php
@@ -1,5 +1,4 @@
 <?php
-    // Will run a Query finding courses then send results with a table to quickFind.php
    require_once('config.php');
 	  if(isset($_POST['code']) {
 	     $code = $_POST['code'];
