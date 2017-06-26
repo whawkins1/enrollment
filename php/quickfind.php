5ab696e462e547ffea30e71bbd351925c37aea51
Bill <hawkins5513@yahoo.com>
2017-06-26 16:18:41 -0400

    Added Styles and position to quickfindstyles.css to position label in quickfind.php

--- php/quickfind.php
+++ php/quickfind.php
@@ -16,11 +16,13 @@
                     <label id="labelinputcodetitle">Enter Course Code:</label>
                     <input type="text" id="inputcoursecode" autofocus>
                 </div>
+				<div id="containerresult">
+				     <label id='resultslabel'> No Results </label>
+                </div>
                 <div id="containerbutton">
                       <button type="button" id="quickfindbutton" disabled>Find Course</button>
                 </div>
-                <div id="containerresult">
-                </div>
+                
           </div>      
               <script src="../plugins/jquery-1.12.0.js"></script>
               <script src="../javascript/quickfind.js"></script>
