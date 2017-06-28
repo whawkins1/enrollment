0c88e2f2c8bd50f79925ed2e92b5aa3d85f2b314
Bill <hawkins5513@yahoo.com>
2017-06-28 18:25:37 -0400

    Changed button position in quickFind.js as well as changing the text 'find course' to a search icon

--- php/quickfind.php
+++ php/quickfind.php
@@ -15,13 +15,10 @@
                 <div id="containerinputcode">
                     <label id="labelinputcodetitle">Enter Course Code:</label>
                     <input type="text" id="inputcoursecode" autofocus>
+					<button type="button" id="quickfindbutton" disabled><img src = "../images/search.png"></button>
                 </div>
 				<div id="containerresult">
                 </div>
-                <div id="containerbutton">
-                      <button type="button" id="quickfindbutton" disabled>Find Course</button>
-                </div>
-                
           </div>      
               <script src="../plugins/jquery-1.12.0.js"></script>
               <script src="../javascript/quickfind.js"></script>
